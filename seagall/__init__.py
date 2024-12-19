6fd7ccddb0bc6078f8e5411db53b40d20bd0609f
gmalagol10 <gabriele.malagoli3@gmail.com>
2024-12-19 14:28:59 +0100

    Initial commit

--- seagall/__init__.py
+++ seagall/__init__.py
@@ -4,7 +4,7 @@ __author__ = ', '.join(['Gabriele Malagoli'])
 
 __version__ = 0.1
 
-import .Utils as ut
-import .ML_utils as mlu
-import .EmbeddExplain as ee
-import .HPO as hpo
+import Utils as ut
+import ML_utils as mlu
+import EmbeddExplain as ee
+import HPO as hpo
