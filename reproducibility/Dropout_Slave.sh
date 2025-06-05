fabf2c0fb92ea9eca3552a6ea12caa3107519fc9
gmalagol10 <gabriele.malagoli3@gmail.com>
2025-06-05 17:10:23 +0200

    Initial commit

--- reproducibility/Dropout_Slave.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ae=$1
-ds=$2
-fs=$3
-
-python Robustness.py $ae $ds $fs
--- reproducibility/Robustness.py
+++ reproducibility/Robustness.py
@@ -15,31 +15,29 @@ import ML_utils as mlu
 
 print(f"Robustness Script started at:", time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()), flush=True)
 
-AE = sys.argv[1]
-dataset = sys.argv[2]
-featurespace = sys.argv[3]
-#dataset = sys.argv[4]
-#featurespace = sys.argv[5]
-#start = sys.argv[6]
+dataset = sys.argv[1]
+featurespace = sys.argv[2]
 
-names = ["Script", "AE", "FS", "Features space"]
+names = ["Script", "Data set", "Features space"]
 for n,arg in zip(names,sys.argv):
 	print(n,arg, flush=True)
 	
 path = f"Datasets/{dataset}/FeatureSpaces/{featurespace}/Dropout"
 matrix = f"Datasets/{dataset}/FeatureSpaces/{featurespace}/CM/{dataset}_{featurespace}_Dropout.h5ad"
-print(time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()), f"Loading {matrix}", flush=True)					 	
+print(time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()), f"Loading {matrix}", flush=True)
 adata = sc.read_h5ad(matrix)
 adata = adata[adata.obs[adata.obs["CellType"].isin(list(adata.obs.value_counts("CellType")[adata.obs.value_counts("CellType")>50].index))].index]
 y = np.array(adata.obs.target).astype(int)
-Path(f"{path}/{AE}").mkdir(parents=True, exist_ok=True)
-for run in range(1, 10):
-	for d in np.linspace(0, 50, 6).astype(int):
 
-		M =	adata.layers[f"X_{str(d)}_{str(run)}"].copy()
+for AE in ["scVI","TopoAE","VAE","PeakVI","GRAE"]:
+	Path(f"{path}/{AE}").mkdir(parents=True, exist_ok=True)
+	for run in range(1, 10):
+		for d in np.linspace(0, 50, 6).astype(int):
 
-		print(run, d, adata.X.getnnz()/(adata.shape[0]*adata.shape[1]), M.getnnz()/(adata.shape[0]*adata.shape[1]), flush=True)
+			M =	adata.layers[f"X_{str(d)}_{str(run)}"].copy()
 
-		Path(f"{path}/{AE}").mkdir(parents=True, exist_ok=True)
-		out = mlu.ApplyAE(M=M, representation=AE, y=y, model_name=f"{path}/{AE}/{dataset}_{featurespace}_{AE}_{str(d)}_{str(run)}")
-		del out
+			print(run, d, adata.X.getnnz()/(adata.shape[0]*adata.shape[1]), M.getnnz()/(adata.shape[0]*adata.shape[1]), flush=True)
+
+			Path(f"{path}/{AE}").mkdir(parents=True, exist_ok=True)
+			out = mlu.ApplyAE(M=M, representation=AE, y=y, model_name=f"{path}/{AE}/{dataset}_{featurespace}_{AE}_{str(d)}_{str(run)}")
+			del out
