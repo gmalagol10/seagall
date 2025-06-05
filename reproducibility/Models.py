23cf1b5af05ce1890491338713ac8838e98a5e1e
gmalagol10 <gabriele.malagoli3@gmail.com>
2025-06-05 17:14:19 +0200

    Initial commit

--- reproducibility/Models.py
+++ reproducibility/Models.py
@@ -56,53 +56,8 @@ class NN(torch.nn.Module):
 		x = self.l2(x)
 		
 		return x
-		
-class BaseAE(torch.nn.Module):
-	def __init__(self, input_dim, hidden_dim, latent_dim, act_fn=torch.nn.LeakyReLU, act_fn_out=None, dp=0.3):
-		"""Create new autoencoder with pre-defined latent dimension."""
-		super().__init__()
-
-		self.input_dim = input_dim
-		self.hidden_dim = hidden_dim
-		self.latent_dim = latent_dim
-																																																																															
-		self.act_fn = act_fn
-		self.act_fn_out = act_fn_out
-
-		self.encoder = torch.nn.Sequential(torch.nn.Dropout(dp), torch.nn.Linear(self.input_dim, self.hidden_dim), self.act_fn(), 
-										   torch.nn.Dropout(dp), torch.nn.Linear(self.hidden_dim, self.latent_dim))
-
-		if act_fn_out:
-				self.decoder = torch.nn.Sequential(torch.nn.Dropout(dp), torch.nn.Linear(self.latent_dim, self.hidden_dim), self.act_fn(),
-												   torch.nn.Dropout(dp), torch.nn.Linear(self.hidden_dim, self.input_dim), self.act_fn_out())
-		else:
-				self.decoder = torch.nn.Sequential(torch.nn.Dropout(dp), torch.nn.Linear(self.latent_dim, self.hidden_dim), self.act_fn(), 
-												   torch.nn.Dropout(dp), torch.nn.Linear(self.hidden_dim, self.input_dim))
-
-		self.loss_fn = torch.nn.MSELoss()
-
-	def encode(self, x):
-		"""Embed data in latent space."""
-		return self.encoder(x)
-
-	def decode(self, z):
-		"""Decode data from latent space."""
-		return self.decoder(z)
-
-	def forward(self, x):
-		"""Embeds and reconstructs data, returning a loss."""
-		z = self.encode(x)
-		x_hat = self.decode(z)
-
-		# The loss can of course be changed. If this is your first time
-		# working with autoencoders, a good exercise would be to 'grok'
-		# the meaning of different losses.
-		reconstruction_error = self.loss_fn(x, x_hat)
-		return reconstruction_error		
-		
+				
 class TopologicallyRegularizedAutoencoder(AutoencoderModel):
-	"""Topological Autoencoder."""
-
 	def __init__(self, lam=1., p=2, autoencoder_model='BaseAE', ae_kwargs=None, toposig_kwargs=None):
 		"""
 			Args:
@@ -157,7 +112,6 @@ class TopologicallyRegularizedAutoencoder(AutoencoderModel):
 	
 class VAutoencoder(torch.nn.Module):
 	def __init__(self, autoencoder_model='BaseAE', ae_kwargs=None):
-		"""Create new autoencoder with pre-defined latent dimension."""
 		super().__init__()
 
 		ae_kwargs = ae_kwargs if ae_kwargs else {}
--- reproducibility/Robustness.py
+++ reproducibility/Robustness.py
@@ -21,7 +21,7 @@ featurespace = sys.argv[2]
 names = ["Script", "Data set", "Features space"]
 for n,arg in zip(names,sys.argv):
 	print(n,arg, flush=True)
-	
+
 path = f"Datasets/{dataset}/FeatureSpaces/{featurespace}/Dropout"
 matrix = f"Datasets/{dataset}/FeatureSpaces/{featurespace}/CM/{dataset}_{featurespace}_Dropout.h5ad"
 print(time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()), f"Loading {matrix}", flush=True)
@@ -34,7 +34,7 @@ for AE in ["scVI","TopoAE","VAE","PeakVI","GRAE"]:
 	for run in range(1, 10):
 		for d in np.linspace(0, 50, 6).astype(int):
 
-			M =	adata.layers[f"X_{str(d)}_{str(run)}"].copy()
+			M = adata.layers[f"X_{str(d)}_{str(run)}"].copy()
 
 			print(run, d, adata.X.getnnz()/(adata.shape[0]*adata.shape[1]), M.getnnz()/(adata.shape[0]*adata.shape[1]), flush=True)
 
