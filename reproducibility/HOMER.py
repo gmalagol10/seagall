bd8a369df0b62c7f2d8769d9c1a70988665117df
gmalagol10 <gabriele.malagoli3@gmail.com>
2026-02-17 14:32:38 +0100

    Initial commit

--- reproducibility/HOMER.py
+++ reproducibility/HOMER.py
@@ -27,33 +27,51 @@ snp2=f"Datasets/{dataset}/FeatureSpaces/{featurespace}/XAI/SnapATAC2/{dataset}_{
 snp2_imp=f"Datasets/{dataset}/FeatureSpaces/{featurespace}/XAI/SnapATAC2/{dataset}_{featurespace}_FeaturesPvsAdj.tsv.gz"
 xai=f"Datasets/{dataset}/FeatureSpaces/{featurespace}/XAI/{dataset}_{featurespace}_GRAE_kNN_{label}_HPO_XAIFeatures.tsv.gz"
 xai_imp=f"Datasets/{dataset}/FeatureSpaces/{featurespace}/XAI/{dataset}_{featurespace}_GRAE_kNN_{label}_HPO_XAIFeatImpCM.tsv.gz"
+de=f"Datasets/{dataset}/FeatureSpaces/{featurespace}/XAI/DE/{dataset}_{featurespace}_GRAE_kNN_{label}_DEFeatures.tsv.gz"
+de_imp=f"Datasets/{dataset}/FeatureSpaces/{featurespace}/XAI/DE/{dataset}_{featurespace}_GRAE_kNN_{label}_DEFeaturesPvsAdj.tsv.gz"
 
 snp2=pd.read_csv(snp2, sep="\t", index_col=0)[:500]
 snp2_imp=pd.read_csv(snp2_imp, sep="\t", index_col=0)
 xai=pd.read_csv(xai, sep="\t", index_col=0)[:500]
 xai_imp=pd.read_csv(xai_imp, sep="\t", index_col=0)
 
+de=pd.read_csv(de, sep="\t", index_col=0)[:500]
+de_imp=pd.read_csv(de_imp, sep="\t", index_col=0)
+
 snp2_path=f"Datasets/{dataset}/FeatureSpaces/{featurespace}/XAI/SnapATAC2/MotifAnalysis/HOMER_SnapATAC2only"
 xai_path=f"../Datasets/{dataset}/FeatureSpaces/{featurespace}/XAI/MotifAnalysis/HOMER_XAIonly"
+de_path=f"../Datasets/{dataset}/FeatureSpaces/{featurespace}/XAI/DE/MotifAnalysis/HOMER_DEonly"
 
 
 Path(snp2_path).mkdir(parents=True, exist_ok=True)
 Path(xai_path).mkdir(parents=True, exist_ok=True)
-inter=intersection([xai.columns, snp2.columns])
+Path(de_path).mkdir(parents=True, exist_ok=True)
+inter=intersection([xai.columns, snp2.columns, de.columns])
 
-for col in inter:
+for col in inter[:1]:
+
+	only_xai=xai_imp[list(set(xai[col]).difference(de[col].dropna()))].mean().sort_values()[::-1][:100]
+	a=list(set(de[col].dropna()).difference(xai[col]))
+	b=pd.concat([pd.DataFrame(de_imp[col]).rename({col : "Imp"}, axis=1), pd.DataFrame(de[col]).rename({col : "Feat"}, axis=1)], axis=1).dropna()
+	only_de=b.sort_values(by="Imp").set_index("Feat").loc[a][:100]
+	
+	print(col, len(only_xai), len(only_de), flush=True)   
+
+	xaiffile=f"{xai_path}/{col}.bed"
+	pd.DataFrame([g.split("_") for g in list(only_xai.index)]).to_csv(xaiffile, sep="\t", header=None, index=None)
+	os.system(f"findMotifsGenome.pl {xaiffile} {genome} {xai_path}/{col} -p 10")
+
+	deffile=f"{de_path}/{col}.bed"
+	pd.DataFrame([g.split("_") for g in list(only_de.index)]).to_csv(deffile, sep="\t", header=None, index=None)
+	os.system(f"findMotifsGenome.pl {deffile} {genome} {de_path}/{col} -p 10")
+
+
 
 	only_xai=xai_imp[list(set(xai[col]).difference(snp2[col].dropna()))].mean().sort_values()[::-1][:100]
 	a=list(set(snp2[col].dropna()).difference(xai[col]))
 	b=pd.concat([pd.DataFrame(snp2_imp[col]).rename({col : "Imp"}, axis=1), pd.DataFrame(snp2[col]).rename({col : "Feat"}, axis=1)], axis=1).dropna()
 	only_snp2=b.sort_values(by="Imp").set_index("Feat").loc[a][:100]
 	print(col, len(only_xai), len(only_snp2), flush=True)   
-
-
-    xaiffile=f"{xai_path}/{col}.bed"
-	pd.DataFrame([g.split("_") for g in list(only_xai.index)]).to_csv(xaiffile, sep="\t", header=None, index=None)
-	os.system(f"findMotifsGenome.pl {xaiffile} {genome} {xai_path}/{col} -p 8")
-
 
 	snp2ffile=f"{snp2_path}/{col}.bed"
 	pd.DataFrame([g.split("_") for g in list(only_snp2.index)]).to_csv(snp2ffile, sep="\t", header=None, index=None)
